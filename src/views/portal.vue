089507583b0af993290408079019bef6db0fe529
LarryKoo <gumutianqi@gmail.com>
2017-09-06 14:27:45 +0800

    learn log

--- src/views/portal.vue
+++ src/views/portal.vue
@@ -1,64 +1,64 @@
 <style scoped>
-    .layout {
+    .layout{
         border: 1px solid #d7dde4;
         background: #f5f7f9;
         position: relative;
+        border-radius: 4px;
+        overflow: hidden;
     }
-
-    .layout-breadcrumb {
+    .layout-breadcrumb{
         padding: 10px 15px 0;
     }
-
-    .layout > .ivu-row-flex {
-        height: inherit;
-    }
-
-    .layout-content {
+    .layout-content{
         min-height: 200px;
         margin: 15px;
         overflow: hidden;
         background: #fff;
         border-radius: 4px;
     }
-
-    .layout-content-main {
+    .layout-content-main{
         padding: 10px;
     }
-
-    /*.layout-copy {*/
-        /*text-align: center;*/
-        /*padding: 10px 0 20px;*/
-        /*color: #9ea7b4;*/
-    /*}*/
-
-    .layout-menu-left {
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
         background: #464c5b;
     }
-
-    .layout-header {
-        height: 60px;
+    .layout-header{
+        height: 50px;
         background: #fff;
-        box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
     }
-
-    .layout-logo-left {
+    .layout-logo-left{
         width: 90%;
         height: 30px;
         background: #5b6270;
         border-radius: 3px;
         margin: 15px auto;
     }
+    .layout-ceiling-main a{
+        color: #9ba7b5;
+    }
+    .layout-hide-text .layout-text{
+        display: none;
+    }
+    .ivu-col{
+        transition: width .2s ease-in-out;
+    }
 </style>
 <template>
-    <div class="layout">
+    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
         <Row type="flex">
-            <i-col span="5" class="layout-menu-left">
-                <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']">
+            <i-col :span="spanLeft" class="layout-menu-left">
+                <i-menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
                     <div class="layout-logo-left"></div>
                     <Submenu name="1">
                         <template slot="title">
-                            <Icon type="ios-navigate"></Icon>
-                            导航一
+                            <Icon type="ios-navigate" :size="iconSize"></Icon>
+                            <span class="layout-text">导航一</span>
                         </template>
                         <Menu-item name="1-1">选项 1</Menu-item>
                         <Menu-item name="1-2">选项 2</Menu-item>
@@ -66,43 +66,74 @@
                     </Submenu>
                     <Submenu name="2">
                         <template slot="title">
-                            <Icon type="ios-keypad"></Icon>
-                            导航二
+                            <Icon type="ios-keypad" :size="iconSize"></Icon>
+                            <span class="layout-text">导航二</span>
                         </template>
                         <Menu-item name="2-1">选项 1</Menu-item>
                         <Menu-item name="2-2">选项 2</Menu-item>
                     </Submenu>
                     <Submenu name="3">
                         <template slot="title">
-                            <Icon type="ios-analytics"></Icon>
-                            导航三
+                            <Icon type="ios-analytics" :size="iconSize"></Icon>
+                            <span class="layout-text">导航三</span>
                         </template>
                         <Menu-item name="3-1">选项 1</Menu-item>
                         <Menu-item name="3-2">选项 2</Menu-item>
                     </Submenu>
-                </Menu>
+                </i-menu>
+
             </i-col>
-            <i-col span="19">
-                <div class="layout-header"></div>
+            <i-col :span="spanRight">
+                <div class="layout-header">
+                    <i-button type="text" @click="toggleClick">
+                        <Icon type="navicon" size="32"></Icon>
+                    </i-button>
+                </div>
                 <div class="layout-breadcrumb">
                     <Breadcrumb>
-                        <Breadcrumb-item>
-                            <router-link to="/">首页</router-link>
-                        </Breadcrumb-item>
+                        <Breadcrumb-item href="#">首页</Breadcrumb-item>
                         <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
                         <Breadcrumb-item>某应用</Breadcrumb-item>
                     </Breadcrumb>
                 </div>
                 <div class="layout-content">
-                    <div class="layout-content-main">内容区域</div>
+                    <div class="layout-content-main" :style="contentStyle">内容区域</div>
                 </div>
-                <!--<div class="layout-copy">-->
-                    <!--2011-2016 &copy; TalkingData-->
-                <!--</div>-->
+                <div class="layout-copy">
+                    2016-2017 &copy; 027wh
+                </div>
             </i-col>
         </Row>
     </div>
 </template>
 <script>
-    export default {};
+    export default {
+        el: '#app',
+        data: {
+            spanLeft: 5,
+            spanRight: 19,
+            theme1: 'dark'
+        },
+        computed:{
+            iconSize:function () {
+                return this.spanLeft === 5 ? 14 : 24;
+            },
+            contentStyle: function(){
+                var innerHeight = window.innerHeight;
+                var contentHeight = innerHeight-50-110;
+                return "min-height: "+contentHeight+"px";
+            }
+        },
+        methods: {
+            toggleClick: function() {
+                if (this.spanLeft === 5) {
+                    this.spanLeft = 2;
+                    this.spanRight = 22;
+                } else {
+                    this.spanLeft = 5;
+                    this.spanRight = 19;
+                }
+            }
+        }
+    };
 </script>
